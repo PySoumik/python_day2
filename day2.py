4723210ee72825a53fed4b016054d48fdd81e9bd
Soumik <satyajit_jbl@yahoo.com>
2025-09-20 17:57:18 +0600

    First commit

---
+++ day2.py
@@ -0,0 +1,26 @@
+first_name = "Soumik"
+last_name = "Ghosh"
+country = "Bangladesh"
+city ="Dhaka"
+age = 12
+print(type(country))
+
+#float
+f = 0.45
+print(type(f))
+
+#int to float
+number = 23
+#print("type of number",type(number))
+#num_float = float(number)615409
+6
+#print("type of num_float",type(num_float),num_float)
+print(float(number))
+
+#float to int
+float = 155547.51654154
+print(int(float))
+
+
+
+
