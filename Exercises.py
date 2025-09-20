f5d7116809e312ce75c1387c5e0ee0cf4bbad4ad
Soumik <satyajit_jbl@yahoo.com>
2025-09-20 18:52:52 +0600

    Day2 completed

---
+++ Exercises.py
@@ -0,0 +1,22 @@
+# m = "Bafuigherh"
+# print(len(m))
+
+# substration
+
+# num_2 = 5
+# num_1 = 2
+# diff = num_2 % num_1
+# print(diff)
+
+print(5%2)
+
+# exponent
+e =5**2
+print(e)
+
+a=int(input("Enter length of Rectangle :"))
+b=int(input("Enter Width of Rectangle :"))
+
+c=2*(a+b)
+print(c)
+
---
+++ datatype.py
@@ -0,0 +1,24 @@
+#length 
+# s =  "Hello ,World"
+# print(len(s))
+
+# first_name = "Soumik"
+# last_name = "Ghosh"
+# country = "Bangladesh"
+# city ="Dhaka"
+# age = 12
+# print(first_name ,last_name ,"live in ",country)
+
+# a,b,c,d = 5,2,48,9
+# print(c)
+
+# #input
+# first_name =input("What is your name :")
+# print(first_name)
+
+# #input in numbers
+a = int(input("Enter a number for a :"))
+b = int(input("Enter a number for b :"))
+c = a+b
+print(c)
+
