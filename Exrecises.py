6846bfc1f342f9ec076fb499c9929f70b20e4c1f
Soumik <satyajit_jbl@yahoo.com>
2025-09-29 00:30:40 +0600

    Exericer

---
+++ Exrecises.py
@@ -0,0 +1,24 @@
+# a =int(input("Enter a number for length"))
+# b =int(input("Enter a number for width"))
+# perimeter =2*(a+b)
+# print(perimeter)
+
+
+
+# letter = 'p'
+# print(len(letter))
+# first_name = "Soumik"
+# last_name =  "Ghosh"
+# full_name = first_name  +  last_name 
+# print(full_name)
+
+l ='python'
+y ='dragon'
+length_python = len(l)
+length_drayon = len(y)
+print(l<y)
+
+g = 'python'
+s = 'Dragon'
+print('on' in g)
+print('on' in s)
