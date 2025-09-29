caa4a40dd93486d100f631500cc4c4330e58e650
Soumik <satyajit_jbl@yahoo.com>
2025-09-29 11:28:42 +0600

    day4

---
+++ day4.py
@@ -0,0 +1,37 @@
+#len
+letter =  'p'
+print(letter)
+
+print(len(letter))
+
+greeting = 'hello, World!'
+
+print(greeting)
+
+print(len(greeting))
+
+# STRING CONCATENATION =
+
+first_name = 'Soumik'
+last_name  = 'Ghosh'  
+space = '   '
+full_name = first_name + space + last_name 
+print(full_name) 
+
+#ESCAPE SQUENCES IN STRING
+
+text = "Line\t1\nLine\t2\tTabbed\nHe said \"Python is intesting!\""
+print(text)
+  
+
+name = 'Soumik'  
+age  = 12
+Inforation = f"My name is {name} and i am {age} years old" 
+print(Inforation)
+
+
+
+
+
+
+
