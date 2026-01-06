3999958de0d7524deacb3fc0271a01db0e2d7629
Soumik <satyajit_jbl@yahoo.com>
2026-01-07 00:06:08 +0600

    print

---
+++ print.py
@@ -0,0 +1,23 @@
+#Join list
+num1 = [1,2,3,]
+num2 = [4,5,6]
+num3 = num1 + num2
+#print(num3)
+#Another way
+num1.extend(num2)
+print(num1)
+#reverse
+num = [1,2,3,4,5,6,7,8,9]
+num.sort(reverse=True)
+print(num)
+#sort
+number = [5,3,1,2,7,9,]
+number.sort()
+print(number)
+#loop
+looplist = ["Samajit","nobo","debdip"]
+for s in looplist :
+    print(s)
+bestfriend = ["Arko","dIPON","NOBO"]
+bestfriend.append("Abir")
+print(bestfriend)    
