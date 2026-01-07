ec4d7dd309d4f96a06a8595b4f2024a795aacf82
Soumik <satyajit_jbl@yahoo.com>
2026-01-07 14:08:58 +0600

    print

--- print.py
+++ print.py
@@ -45,3 +45,11 @@ print(type(num2))
 
 n = True
 print(type(n))
+
+length = int(input("Enter the length of the rectangle: "))
+width = int(input("Enter the width of the rectangle: "))
+
+area = length * width
+
+print(f"The area of the rectangle is {area}")
+
