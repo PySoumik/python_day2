363e9801e361f4bee6f5aa2e42681d22bf022afc
Soumik <satyajit_jbl@yahoo.com>
2026-02-07 15:22:16 +0600

    if,else

--- Tuple.py
+++ Tuple.py
@@ -1,18 +1,28 @@
-friend = ("Sammak","Riddhi","Nafis","Bisanat", "uuu")
-# a = list(friend)
-# a.append("Adriyan")
-# friend = tuple(a)
-# print(friend)
-for x in friend:
-    print(x)
-for i in range(len(friend)):
-     print(i)
+# friend = ("Sammak","Riddhi","Nafis","Bisanat", "uuu")
+# # a = list(friend)
+# # a.append("Adriyan")
+# # friend = tuple(a)
+# # print(friend)
+# for x in friend:
+#     print(x)
+# for i in range(len(friend)):
+#      print(i)
 
 
-num1 = (2,4,5,8,7)
-num2 =  num1 * 2
-print(num2)
+# num1 = (2,4,5,8,7)
+# num2 =  num1 * 2
+# print(num2)
 # num2 = (1, 2, 3)
 
 # num3 = num1 + num2
 # print(num3) 
+
+
+friend = ("Sammak","Riddhi","Nafis","Bisanat", "uuu")
+x = friend.index("Nafis")
+print(x)
+
+
+friend = ("Sammak","Riddhi","Nafis","Bisanat", "uuu","Sammak","Riddhi","Nafis","Bisanat", "uuu""Sammak","Riddhi","Nafis","Bisanat", "uuu")
+s = friend.count("uuu")
+print(s)
---
+++ if,else.py
@@ -0,0 +1,40 @@
+# if 10>5 :
+#     print("10 is grater than 5")
+# print("python is interesting")
+
+# # 2
+# x =int(input("Enter a number "))
+# if x == 6:
+#     print("x equal 6")
+# else:
+#     print("hgrhruh")
+
+# 3
+alpabate = "A"  
+if alpabate == "B" :
+    print("the letter is B")
+else:
+    if alpabate =="C":
+        print("The letter is C")
+    else :    
+        if alpabate == "A":
+            print("The letter is A")
+        else:
+            print("f v f v hsff g")  
+
+# 4
+a = 12
+if a>10:
+    print("jfnrun")
+    if a < 15:
+        print("Sammak")   
+
+# 5
+letter = "A"
+if letter == "B":
+    print("gbfdhvu")
+elif letter == "C" :
+     print("h fb v")
+elif letter == "A":
+    print("fuvdebfj")     
+                    
